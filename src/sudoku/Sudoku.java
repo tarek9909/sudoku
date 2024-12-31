dd556d529a1485cd4c69e8d46957fc976c28ea46
Dell <mohamadmakary33@gmail.com>
2024-12-31 03:58:23 +0200

    first commit

---
+++ src/sudoku/Sudoku.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sudoku;
+import sudoku.Easy_Mode;
+import java.util.Random;
+
+/**
+ *
+ * @author abdallah
+ */
+public class Sudoku extends Easy_Mode {
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        Easy_Mode classinstance;
+        classinstance = new Easy_Mode();
+        classinstance.main(args);
+    }
+    
+}
